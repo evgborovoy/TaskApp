ae3c7f5bf114483e930428609ce07ad92cc68110
Evgeniy <borovoyevgen@gmail.com>
2024-12-09 17:16:14 +0400

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by Evgeniy Borovoy on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
